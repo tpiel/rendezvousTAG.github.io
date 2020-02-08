bdcc2992309b134847f73ae2610b2bbb225a006d
Yonathan Kebede <kebedey16@gmail.com>
2020-02-08 13:28:15 -0500

    added script to populate tables with test data

---
+++ db_scripts/populate_tables.sql
@@ -0,0 +1,15 @@
+INSERT INTO users VALUES
+  ('andre3', '8437435175', 'Andre Robinson'),
+  ('tyler', '1234567890', 'Tyler Piel'),
+  ('frank', '2222222222', 'Frank Wu');
+
+INSERT INTO availability (username, available) VALUES
+  ('andre3', '20200208 12:00:00 PM'),
+  ('andre3', '20200208 12:15:00 PM'),
+  ('andre3', '20200208 12:30:00 PM'),
+  ('tyler', '20200208 12:15:00 PM'),
+  ('tyler', '20200208 12:30:00 PM'),
+  ('tyler', '20200208 12:45:00 PM'),
+  ('frank', '20200208 12:30:00 PM'),
+  ('frank', '20200208 12:45:00 PM'),
+  ('frank', '20200208 01:00:00 PM');
